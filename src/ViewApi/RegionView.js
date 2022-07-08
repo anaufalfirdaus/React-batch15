47d0ad8bfc52f17c4863fcc9bba63a9de5e9ae86
naufalfirdaus <naufalfirdausa@gmail.com>
2022-07-08 18:39:20 +0700

    day07

--- src/ViewApi/RegionView.js
+++ src/ViewApi/RegionView.js
@@ -1,83 +1,98 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import regionApi from '../api/regionApi'
 import RegionAdd from './RegionAdd'
 
 export default function RegionView() {
- const [region,setRegion] = useState([])
- const [display,setDisplay] = useState(false)
- const [refresh,setRefresh] = useState(false)
- const [values,setValues] = useState ({
-    region_id : undefined,
-    region_name : '',
-}) 
+    const [region, setRegion] = useState([])
+    const [display, setDisplay] = useState(false)
+    const [refresh, setRefresh] = useState(false)
+    const [values, setValues] = useState({
+        region_id: undefined,
+        region_name: '',
+    })
     useEffect(() => {
         regionApi.list().then(data => {
             setRegion(data)
         })
         setRefresh(false)
-    },[refresh])
+    }, [refresh])
 
     const handleOnChange = name => event => {
         setValues({ ...values, [name]: event.target.value })
     }
 
-    const onSubmit = async() => {
+    const onSubmit = async () => {
         const payload = {
-            region_name : (values.region_name)
+            region_name: (values.region_name)
         }
-        
+
         await regionApi.create(payload)
-        .then(()=>{
-            setDisplay(false) 
-            setRefresh(true)
-           window.alert('Data Successfully Insert')
-        })
-      
+            .then(() => {
+                setDisplay(false)
+                setRefresh(true)
+                window.alert('Data Successfully Insert')
+            })
+
     }
-    const onDelete = async(id) => {
+    const onDelete = async (id) => {
         regionApi.deleted(id)
-        .then(()=>{
-            setRefresh(true)
-            window.alert('Data Successfully Delete')
-        })
+            .then(() => {
+                setRefresh(true)
+                window.alert('Data Successfully Delete')
+            })
     }
 
-  return (
-    <div>
+    return (
         <div>
+            <div>
                 <h2>List Region</h2>
                 <button onClick={() => setDisplay(true)}> Add Region </button>
                 {
                     display ?
-                    <RegionAdd
-                    onSubmit = {onSubmit}
-                    handleOnChange = {handleOnChange}
-                    setDisplay = {setDisplay}
-                    /> 
-                    :
+                        <RegionAdd
+                            onSubmit={onSubmit}
+                            handleOnChange={handleOnChange}
+                            setDisplay={setDisplay}
+                        />
+                        :
                         <>
                             <table>
                                 <th>Region ID</th>
                                 <th>Region Name</th>
-                                <th>Country ID</th>
+                                <th>Country</th>
                                 <tbody>
                                     {
-                                        region&&region.map( reg => (
-                                            <tr key={reg.region_id}>
-                                                <td>{reg.region_id}</td>
-                                                <td>{reg.region_name}</td>
-                                                <td>{reg && reg.countries.map(coun => (
-                                                    coun.country_name
-                                                ))}</td>
-                                                <button onClick={() => onDelete(reg.region_id)}> Delete Region </button>
-                                            </tr>
-                                        ))
+                                        region && region.map(reg => {
+                                            return (
+                                                <tr key={reg.region_id}>
+                                                    <td>{reg.region_id}</td>
+                                                    <td>{reg.region_name}</td>
+                                                    <table>
+                                                        <th>Country ID </th>
+                                                        <th> Country Name</th>
+                                                        <tbody>
+                                                            {reg && reg.countries.map(coun => {
+                                                                return (
+                                                                <tr key={coun.country_id}>
+                                                                <td>{coun.country_id}</td>
+                                                                <td>{coun.country_name}</td>
+                                                                </tr>
+                                                            )})}
+                                                        </tbody>
+
+
+                                                    </table>
+                                                    <button onClick={() => onDelete(reg.region_id)}> Delete Region </button>
+                                                </tr>
+                                            )
+                                        })
                                     }
+
                                 </tbody>
                             </table>
                         </>
                 }
             </div>
-    </div>
-  )
+        </div>
+    )
 }
